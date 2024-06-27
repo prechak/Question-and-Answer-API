182be3eb8f5bdfbaaa94fc260b31bc3dfdd4a35c
Prechak <benedict.precha@gmail.com>
2024-06-27 09:55:51 +0700

    feat: create post and get api

--- app.mjs
+++ app.mjs
@@ -10,12 +10,40 @@ app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
 });
 
+//db connection test
 app.get("/dbtest", async (req, res) => {
   let result = await connectionPool.query(`select * from questions`);
 
   return res.status(200).json({
-    data: result.rows,
     message: "Database is connected 🐘",
+  });
+});
+
+//ผู้ใช้งานสามารถสร้างคำถามได้
+app.post("/questions", async (req, res) => {
+  const newQuestion = {
+    ...req.body,
+  };
+
+  await connectionPool.query(
+    `
+    insert into questions (title, description, category)
+    values($1, $2, $3)
+    `,
+    [newQuestion.title, newQuestion.description, newQuestion.category]
+  );
+
+  return res.status(201).json({
+    message: "Created: Question created successfully.",
+  });
+});
+
+//ผู้ใช้งานสามารถที่จะดูคำถามทั้งหมดได้
+app.get("/questions", async (req, res) => {
+  let result = await connectionPool.query(`select * from questions`);
+
+  return res.status(200).json({
+    data: result.rows,
   });
 });
 
