f8d55143378a327376083a349088ffc5c9cb49b2
Prechak <benedict.precha@gmail.com>
2024-06-28 01:38:22 +0700

    feat: move answer path to express router

--- app.mjs
+++ app.mjs
@@ -1,12 +1,16 @@
 import express from "express";
-import connectionPool from "./utils/db.mjs";
+
 import questionRouter from "./routes/question.mjs";
+import answerRouter from "./routes/answer.mjs";
 
 const app = express();
 const port = 7777;
 
 app.use(express.json());
 app.use("/questions", questionRouter);
+app.use("/answers", answerRouter);
+
+//ไม่ได้เพิ่ม Timestamp เพราะ pgAdmin gen ให้อยู่แล้ว
 
 /*
 app.get("/test", (req, res) => {
@@ -22,88 +26,6 @@ app.get("/dbtest", async (req, res) => {
   });
 });
 */
-
-//ผู้ใช้งานสามารถกดปุ่มเห็นด้วย หรือไม่เห็นด้วยกับคำตอบได้ - Answer Upvote
-app.post("/answers/:id/upvote", async (req, res) => {
-  const answerId = req.params.id;
-  let newVote = { ...req.body };
-  let checkAnswer;
-  let result;
-
-  try {
-    if (!Number(newVote.vote) || newVote.vote !== 1) {
-      return res.status(400).json({
-        message: `Vote must be number and value must be 1`,
-      });
-    }
-    checkAnswer = await connectionPool.query(
-      `select * from answers where id = $1`,
-      [answerId]
-    );
-
-    if (!checkAnswer.rows[0]) {
-      return res.status(404).json({
-        message: `Answer not found.`,
-      });
-    }
-
-    result = await connectionPool.query(
-      `insert into answer_votes (answer_id, vote)
-    values($1, $2) returning *`,
-      [answerId, newVote.vote]
-    );
-  } catch {
-    return res.status(400).json({
-      message: `Missing or invalid request data`,
-    });
-  }
-
-  return res.status(200).json({
-    data: checkAnswer.rows,
-    message: `Successfully upvoted the answers.`,
-  });
-});
-
-//ผู้ใช้งานสามารถกดปุ่มเห็นด้วย หรือไม่เห็นด้วยกับคำตอบได้ - Answer Downvote
-app.post("/answers/:id/downvote", async (req, res) => {
-  const answerId = req.params.id;
-  let newVote = { ...req.body };
-  let checkAnswer;
-  let result;
-
-  try {
-    if (!Number(newVote.vote) || newVote.vote !== -1) {
-      return res.status(400).json({
-        message: `Vote must be number and value must be -1`,
-      });
-    }
-    checkAnswer = await connectionPool.query(
-      `select * from answers where id = $1`,
-      [answerId]
-    );
-
-    if (!checkAnswer.rows[0]) {
-      return res.status(404).json({
-        message: `Answer not found.`,
-      });
-    }
-
-    result = await connectionPool.query(
-      `insert into answer_votes (answer_id, vote)
-    values($1, $2) returning *`,
-      [answerId, newVote.vote]
-    );
-  } catch {
-    return res.status(400).json({
-      message: `Missing or invalid request data`,
-    });
-  }
-
-  return res.status(200).json({
-    data: checkAnswer.rows,
-    message: `Successfully upvoted the answers.`,
-  });
-});
 
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
---
+++ routes/answer.mjs
@@ -0,0 +1,88 @@
+import { Router } from "express";
+import connectionPool from "../utils/db.mjs";
+
+const answerRouter = Router();
+
+//ผู้ใช้งานสามารถกดปุ่มเห็นด้วย หรือไม่เห็นด้วยกับคำตอบได้ - Answer Upvote
+answerRouter.post("/:id/upvote", async (req, res) => {
+  const answerId = req.params.id;
+  let newVote = { ...req.body };
+  let checkAnswer;
+  let result;
+
+  try {
+    if (!Number(newVote.vote) || newVote.vote !== 1) {
+      return res.status(400).json({
+        message: `Vote must be number and value must be 1`,
+      });
+    }
+    checkAnswer = await connectionPool.query(
+      `select * from answers where id = $1`,
+      [answerId]
+    );
+
+    if (!checkAnswer.rows[0]) {
+      return res.status(404).json({
+        message: `Answer not found.`,
+      });
+    }
+
+    result = await connectionPool.query(
+      `insert into answer_votes (answer_id, vote)
+    values($1, $2) returning *`,
+      [answerId, newVote.vote]
+    );
+  } catch {
+    return res.status(400).json({
+      message: `Missing or invalid request data`,
+    });
+  }
+
+  return res.status(200).json({
+    data: checkAnswer.rows,
+    message: `Successfully upvoted the answers.`,
+  });
+});
+
+//ผู้ใช้งานสามารถกดปุ่มเห็นด้วย หรือไม่เห็นด้วยกับคำตอบได้ - Answer Downvote
+answerRouter.post("/:id/downvote", async (req, res) => {
+  const answerId = req.params.id;
+  let newVote = { ...req.body };
+  let checkAnswer;
+  let result;
+
+  try {
+    if (!Number(newVote.vote) || newVote.vote !== -1) {
+      return res.status(400).json({
+        message: `Vote must be number and value must be -1`,
+      });
+    }
+    checkAnswer = await connectionPool.query(
+      `select * from answers where id = $1`,
+      [answerId]
+    );
+
+    if (!checkAnswer.rows[0]) {
+      return res.status(404).json({
+        message: `Answer not found.`,
+      });
+    }
+
+    result = await connectionPool.query(
+      `insert into answer_votes (answer_id, vote)
+    values($1, $2) returning *`,
+      [answerId, newVote.vote]
+    );
+  } catch {
+    return res.status(400).json({
+      message: `Missing or invalid request data`,
+    });
+  }
+
+  return res.status(200).json({
+    data: checkAnswer.rows,
+    message: `Successfully upvoted the answers.`,
+  });
+});
+
+export default answerRouter;
