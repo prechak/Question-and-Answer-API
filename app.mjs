377b9abd4dd4ed4513fbcab62cd48a6b51ea3fdc
Prechak <benedict.precha@gmail.com>
2024-06-28 01:22:38 +0700

    create complete upvote and downvote for question and answer meet optional requirement

--- app.mjs
+++ app.mjs
@@ -23,6 +23,88 @@ app.get("/dbtest", async (req, res) => {
 });
 */
 
+//ผู้ใช้งานสามารถกดปุ่มเห็นด้วย หรือไม่เห็นด้วยกับคำตอบได้ - Answer Upvote
+app.post("/answers/:id/upvote", async (req, res) => {
+  const answerId = req.params.id;
+  let newVote = { ...req.body };
+  let checkAnswer;
+  let result;
+
+  try {
+    if (!Number(newVote.vote) || newVote.vote !== 1) {
+      return res.status(400).json({
+        message: `Vote must be number and value must be 1`,
+      });
+    }
+    checkAnswer = await connectionPool.query(
+      `select * from answers where id = $1`,
+      [answerId]
+    );
+
+    if (!checkAnswer.rows[0]) {
+      return res.status(404).json({
+        message: `Answer not found.`,
+      });
+    }
+
+    result = await connectionPool.query(
+      `insert into answer_votes (answer_id, vote)
+    values($1, $2) returning *`,
+      [answerId, newVote.vote]
+    );
+  } catch {
+    return res.status(400).json({
+      message: `Missing or invalid request data`,
+    });
+  }
+
+  return res.status(200).json({
+    data: checkAnswer.rows,
+    message: `Successfully upvoted the answers.`,
+  });
+});
+
+//ผู้ใช้งานสามารถกดปุ่มเห็นด้วย หรือไม่เห็นด้วยกับคำตอบได้ - Answer Downvote
+app.post("/answers/:id/downvote", async (req, res) => {
+  const answerId = req.params.id;
+  let newVote = { ...req.body };
+  let checkAnswer;
+  let result;
+
+  try {
+    if (!Number(newVote.vote) || newVote.vote !== -1) {
+      return res.status(400).json({
+        message: `Vote must be number and value must be -1`,
+      });
+    }
+    checkAnswer = await connectionPool.query(
+      `select * from answers where id = $1`,
+      [answerId]
+    );
+
+    if (!checkAnswer.rows[0]) {
+      return res.status(404).json({
+        message: `Answer not found.`,
+      });
+    }
+
+    result = await connectionPool.query(
+      `insert into answer_votes (answer_id, vote)
+    values($1, $2) returning *`,
+      [answerId, newVote.vote]
+    );
+  } catch {
+    return res.status(400).json({
+      message: `Missing or invalid request data`,
+    });
+  }
+
+  return res.status(200).json({
+    data: checkAnswer.rows,
+    message: `Successfully upvoted the answers.`,
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
 });
