632ab156f86e4e0bce7d801eab50f24c0d9542c8
Prechak <benedict.precha@gmail.com>
2024-06-27 09:38:43 +0700

    setup and connect db success

--- app.mjs
+++ app.mjs
@@ -1,12 +1,22 @@
 import express from "express";
+import connectionPool from "./utils/db.mjs";
 
 const app = express();
-const port = 4000;
+const port = 7777;
 
 app.use(express.json());
 
 app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
+});
+
+app.get("/dbtest", async (req, res) => {
+  let result = await connectionPool.query(`select * from questions`);
+
+  return res.status(200).json({
+    data: result.rows,
+    message: "Database is connected 🐘",
+  });
 });
 
 app.listen(port, () => {
--- utils/db.mjs
+++ utils/db.mjs
@@ -3,8 +3,8 @@ import * as pg from "pg";
 const { Pool } = pg.default;
 
 const connectionPool = new Pool({
-  connectionString:
-    "postgresql://your-db-username:your-db-password@localhost:5432/your-db-name",
+  connectionString: "postgresql://postgres:std5511pe@localhost:5432/my-quora",
+  // "postgresql://your-db-username:your-db-password@localhost:5432/your-db-name",
 });
 
 export default connectionPool;
